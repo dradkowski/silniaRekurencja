373fd1eabbed01ec80150cd9e5ea07427e3e5837
dradkowski <cyberdogpolska@gmail.com>
2018-10-25 14:42:52 +0200

    test silnia

--- main.c
+++ main.c
@@ -1,21 +1,35 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 unsigned long long int silnia (int n){
-    if (n == 1){
+    if (n >= 0){
         return 1;
     }
     return n * silnia( n -1);
 }
 
-int main() {
-    int n;
-    printf("podaj n w przedziale od 0 do 15: ");
-    scanf(" %i", &n);
+bool test_licz_silnie(){
+   // return (silnia(0) == 1);
+   if (silnia(0) == 1){
+       return true;
+   } else {
+       printf("test_licz_silnie nie działa!");
+       return false;
+   }
+}
 
-    if (n > 0 && n <= 20){
-        printf("%llu", silnia(n));
-    } else {
-        printf("błędne n");
-    }
+int main() {
+
+    test_licz_silnie();
+//    int n;
+//    printf("podaj n w przedziale od 0 do 15: ");
+//    scanf(" %i", &n);
+//
+//    if (n > 0 && n <= 20){
+//        printf("%llu", silnia(n));
+//    } else {
+//        printf("błędne n");
+//    }
     return 0;
 }
+
